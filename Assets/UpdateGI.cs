a19adeb45803c602c3dd03fce958d7e154738240
AdamBaileyAIE <adambailey@aie.edu.au>
2018-10-25 14:14:25 +1030

    Initial Commit - No video file provided

---
+++ Assets/UpdateGI.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpdateGI : MonoBehaviour {
+
+    Renderer rend;
+    bool updateGI = true;
+
+    private void Start()
+    {
+        rend = GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (updateGI)
+            rend.UpdateGIMaterials();
+	}
+}
